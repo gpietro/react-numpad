c548e71fd409770fb4a123a7c0315e97a94f6bb8
Pietro Ghezzi <pietro.ghezzi@eoc.ch>
2017-10-04 16:38:48 +0200

    add storybook, moved components in elements folder, add Time component wrapper

---
+++ stories/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { Button, Welcome } from '@storybook/react/demo';
+
+import NumPad from '../lib';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('NumPad', module)
+  .add('with number', () => <NumPad.Number />)
+  .add('with time', () => <NumPad.Time />)

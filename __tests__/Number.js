4272b12c7e466f42d8298889608b2a254ee40e33
Pietro Ghezzi <pietro.ghezzi@eoc.ch>
2017-11-16 10:03:31 +0100

    update test

--- __tests__/Number.js
+++ __tests__/Number.js
@@ -11,35 +11,36 @@ describe('Number', () => {
   describe('#render()', () => {
     it('renders <InputField />', () => {
       const wrapper = shallow(
-        <Number onChange={(value) => { console.log('value', value)}} label={'Totale'} />
+        <Number onChange={(value) => { console.log('value', value)}} label={'Total'} />
       )
       expect(wrapper.find('InputField')).to.have.length(1)
+      expect(wrapper.find('InputField').props().label).to.equal('Total');
       expect(wrapper.find('InputField').dive().find('Input')).to.have.length(1)
       expect(wrapper.find('InputField').dive().find('Label')).to.have.length(1)
     })
+
+    it('renders <KeyPad />', () => {
+      const wrapper = mount(
+        <Number onChange={(value) => { console.log('value', value)}} />
+      )
+      expect(wrapper.state().show).to.equal(false)
+      expect(wrapper.find('KeyPad')).to.have.length(0)
+      expect(wrapper.state().value).to.equal('')
+      wrapper.find('.numpad-input-value').simulate('click')
+      expect(wrapper.state().show).to.equal(true)
+      expect(wrapper.find('KeyPad')).to.have.length(1)      
+    })
+  })
+
+  describe('#input()', () => {
+    const wrapper = mount(
+      <Number onChange={(value) => { console.log('value', value)}} />
+    )
+    wrapper.find('.numpad-input-value').simulate('click')
+    expect(wrapper.find('Button')).to.have.length(12)
+    wrapper.find('Button').first().simulate('click')
+    wrapper.find('.numpad-input-value').simulate('keyDown', {key: 'Enter'})
+    expect(wrapper.state().show).to.equal(false)
+    expect(wrapper.state().value).to.equal('7')
   })
 })
-
-/*test('Keypad show after input click', () => {
-  let component = mount(
-    
-  )
-  let tree = renderer.create(component).toJSON()
-  expect(tree).toMatchSnapshot()
-
-  expect(component.find('input').length).toEqual(1)
-  expect(component.find('label').length).toEqual(1)
-
-  expect(component.find('KeyPad').length).toEqual(0)
-
-  component.find('input').simulate('click')
-  
-  expect(component.find('KeyPad').length).toEqual(1)
-  
-  
-  expect(component.find('Button').length).toEqual(12)
-  
-  
-  //tree = renderer.create(shallowWithTheme(component.find('Button'))).toJSON()
-  //expect(tree).toMatchSnapshot()
-});*/

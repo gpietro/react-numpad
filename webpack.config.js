a05cf8ca40b91436d24f436ea4fb4621c2dbf855
Pietro Ghezzi <pietro.ghezzi@eoc.ch>
2018-08-29 09:49:31 +0200

    update dependencies: materiial-ui, babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader', // 'babel-loader' is also a valid name to reference
-        query: {
-          presets: ['es2015', 'react'],
-        },
       },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       {

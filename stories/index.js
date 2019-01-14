b175a86fc23fb4e407b7045736193602d0db91b7
David Nussio <david.nussio@eoc.ch>
2019-01-14 12:09:59 +0100

    Fix validators

--- stories/index.js
+++ stories/index.js
@@ -3,19 +3,37 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import styled from 'styled-components';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
+import { specs, describe, it } from 'storybook-addon-specifications';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { expect } from 'chai';
 
-import { NumPad, NumberEditor, positiveIntegerValidation, Time, DateTime } from '../lib';
+import { Button } from '@material-ui/core';
+import {
+  NumPad,
+  NumberEditor,
+  positiveIntegerValidation,
+  CalendarDate,
+  DateTime,
+  Appointment,
+} from '../lib';
 import Modal from './DemoModal';
-import { appointmentDates, LoremIpsum } from './data';
+import { appointmentDates } from './data';
+
+configure({ adapter: new Adapter() });
 
 const oddValidator = value =>
   parseInt(value, 10) > 0 && parseInt(value, 10) % 2 !== 0 && parseFloat(value) % 1 === 0;
 
-storiesOf('Number Editor', module)
+storiesOf('Number', module)
   .addDecorator(withKnobs)
-  .add('Input number', () => {
+  .add('default', () => (
+    <NumPad onChange={action('onChange')} position="startBottomLeft" label="Number">
+      <NumberEditor />
+    </NumPad>
+  ))
+  .add('initial value', () => {
     const value = number('Default value', 70, { range: true, min: 0, max: 90, step: 5 });
     return (
       <NumPad onChange={action('onChange')} position="startBottomLeft" label="Number" value={value}>
@@ -23,29 +41,44 @@ storiesOf('Number Editor', module)
       </NumPad>
     );
   })
-  .add('Input positive integer', () => (
+  .add('positive integer', () => (
     <NumPad onChange={action('onChange')} value="" position="startBottomLeft" label="Number">
-      <NumberEditor keyValid={positiveIntegerValidation} />
+      <NumberEditor keyValidator={positiveIntegerValidation} />
     </NumPad>
   ))
-  .add('Odd numbers', () => (
+  .add('odd numbers', () => (
     <NumPad onChange={action('onChange')} value="" position="startBottomLeft" label="Number">
-      <NumberEditor keyValid={oddValidator} />
+      <NumberEditor keyValidator={oddValidator} />
     </NumPad>
   ))
-  .add('Custom input field', () => (
+  .add('custom input field', () => (
     <NumPad
       onChange={action('onChange')}
       value=""
       position="startBottomLeft"
-      customInput={<TextField variant="outlined" />}
+      customInput={<TextField label="Number" variant="outlined" />}
     >
-      <NumberEditor keyValid={oddValidator} />
+      <NumberEditor />
     </NumPad>
-  ));
+  ))
+  .add('testing numbers', () => {
+    const story = (
+      <NumPad onChange={action('onChange')} value="" position="startBottomLeft" label="Number">
+        <NumberEditor />
+      </NumPad>
+    );
+    specs(() =>
+      describe('Check text', () => {
+        it('Should have the Number label', () => {
+          const wrapper = mount(story);
+          expect(wrapper.text()).equal('Number');
+        });
+      })
+    );
+    return story;
+  });
 
 storiesOf('Date Time Editor', module)
-  .addDecorator(withKnobs)
   .add('Datetime', () => (
     <NumPad
       onChange={action('onChange')}
@@ -63,3 +96,72 @@ storiesOf('Date Time Editor', module)
       </NumPad>
     );
   });
+
+storiesOf('Calendar Editor', module).add('Calendar', () => (
+  <NumPad onChange={action('onChange')} position="startButtomLeft" placeholder="DD-MM-YYYY">
+    <CalendarDate dateFormat="DD-MM-YYYY" />
+  </NumPad>
+));
+
+storiesOf('Appointment Editor', module)
+  .add('default', () => (
+    <NumPad onChange={action('onChange')} position="startButtomLeft" placeholder="DD-MM-YYYY">
+      <Appointment dates={appointmentDates} dateFormat="DD-MM-YYYY" />
+    </NumPad>
+  ))
+  .add('fullscreen', () => (
+    <NumPad
+      onChange={action('onChange')}
+      position="fullscreen"
+      locale="it"
+      placeholder="DD-MM-YYYY"
+    >
+      <Appointment dates={appointmentDates} dateFormat="DD-MM-YYYY" />
+    </NumPad>
+  ));
+
+storiesOf('Modal', module).add('Inside modal', () => (
+  <Modal>
+    <h4>Test component inside a modal</h4>
+    <NumPad
+      style={{ fontSize: '10px' }}
+      key="number-1"
+      placeholder="test"
+      theme="orange"
+      onChange={action('selected value')}
+      position="startBottomLeft"
+      label="Totale"
+      value={10}
+      customInput={
+        <div>
+          <TextField label="Uncontrolled" margin="normal" variant="outlined" value="1234" />
+          <Button>open</Button>
+        </div>
+      }
+    >
+      <NumberEditor />
+    </NumPad>
+    {/* <hr />
+      <NumPad.Time
+        key="time-2"
+        theme="blackAndWhite"
+        onChange={value => console.log('changed', value)}
+      />
+      <hr />
+      <NumPad.Calendar
+        onChange={value => console.log('changed', value)}
+        label={'Data di nascita'}
+        locale="it"
+        dateFormat="DD.MM.YYYY"
+        value={'28.06.1986'}
+      />
+      <hr />
+      <NumPad.Calendar
+        onChange={value => console.log('changed', value)}
+        label={'Markers'}
+        locale="it"
+        dateFormat="DD.MM.YYYY"
+        markers={['01.03.2018', '06.03.2018']}
+      /> */}
+  </Modal>
+));
